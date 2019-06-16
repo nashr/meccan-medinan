00396563339032f361c44135a2900a0d23b9c29f
nashr <muhammad.nassirudin@gmail.com>
2019-06-16 21:58:12 +0700

    init'd script to extract features

--- makefile
+++ makefile
@@ -1,2 +1,5 @@
 get_dataset: src/data/get_raw_data.py
 	python src/data/get_raw_data.py
+
+cleanup_feature: src/data/cleanup_feature.py
+	python src/data/cleanup_feature.py
---
+++ src/data/cleanup_feature.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+print("Loading required data...")
+df_raw = pd.read_csv("data/processed/suras.csv")
+
+df = pd.DataFrame()
+
+print("Extracting feature: is_prefix_al...")
+al = chr(1575) + chr(1604)
+df["is_prefix_al"] = df_raw.name.str.startswith(al)
+
+print("Extracting feature: is_moon...")
+suns = [
+    "ت", "ث", "د", "ذ", "ر", "ز",
+    "س", "ش", "ص", "ض", "ط", "ظ",
+    "ل", "ن",
+]
+moons = [
+    "ء", "ا", "آ", "أ", "إ", "ب",
+    "ج", "ح", "خ", "ع", "غ", "ف",
+    "ق", "ك", "م", "و", "ي", "ه",
+    "ة",
+]
+df["is_moon"] = np.where(
+    df.is_prefix_al,
+    df_raw.name.str[2],
+    df_raw.name.str[0]
+)
+df.is_moon = np.where(
+    df.is_moon.isin(moons),
+    "True",
+    np.where(
+        df.is_moon.isin(suns),
+        "False",
+        np.nan
+    )
+)
+
+print("Encoding target: type -> is_meccan...")
+df["is_meccan"] = (df_raw.type == "Meccan")
+
+print("Saving to CSV...")
+df.to_csv("data/processed/feature.csv", index=False)
